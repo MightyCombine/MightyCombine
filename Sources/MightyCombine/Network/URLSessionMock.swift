52390a40b5f21b122621c7d3534b9b6202f3e1a1
insub <vb15@kakao.com>
2023-10-05 20:40:54 +0900

    Mock DI

--- Sources/MightyCombine/Network/URLSessionMock.swift
+++ Sources/MightyCombine/Network/URLSessionMock.swift
@@ -8,10 +8,48 @@
 import Foundation
 import Combine
 
-public struct MockURLSession: URLSessionable {
+//public struct MockURLSession: URLSessionable {
+//    
+//    public func request<T>(_ urlRequest: URLRequest) -> AnyPublisher<T, Error> where T : Decodable {
+//        Empty()
+//            .eraseToAnyPublisher()
+//    }
+//}
+
+public struct MockURLSession: URLSessionable, Mockable {
+    
+    public var mock: Mock
+    
+    init(mock: Mock) {
+        self.mock = mock
+    }
     
     public func request<T>(_ urlRequest: URLRequest) -> AnyPublisher<T, Error> where T : Decodable {
-        Empty()
-            .eraseToAnyPublisher()
+        if let data = mock.data {
+            do {
+                let decoded = try JSONDecoder().decode(T.self, from: data)
+                return Just(decoded)
+                    .setFailureType(to: Error.self)
+                    .eraseToAnyPublisher()
+            } catch let error {
+                return Fail(error: error)
+                    .eraseToAnyPublisher()
+            }
+        } else if let error = mock.error {
+            return Fail(error: error)
+                .eraseToAnyPublisher()
+        } else {
+            return Empty()
+                .eraseToAnyPublisher()
+        }
     }
+}
+
+public protocol Mockable {
+    var mock: Mock { get }
+}
+
+public struct Mock {
+    var data: Data?
+    var error: Error?
 }
