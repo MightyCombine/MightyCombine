014df2911418c1e48492d75efa6efa3de4e39b6d
insub <vb15@kakao.com>
2023-10-05 16:03:57 +0900

    Fix

--- Sources/PowerfulCombine/Extensions/AnyPublisher+.swift
+++ Sources/PowerfulCombine/Extensions/AnyPublisher+.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Combine
 
-public extension AnyPublisher where Failure: Error {
+public extension AnyPublisher {
 
      var asyncThrows: Output {
         get async throws {
