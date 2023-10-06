85beed2cb35a2b7385f665cbedd381949f2a451d
insub <vb15@kakao.com>
2023-10-06 17:44:15 +0900

    [File] Subscribers+ 추가

---
+++ Sources/MightyCombine/Extensions/Subscribers+.swift
@@ -0,0 +1,19 @@
+//
+//  Subscribers+.swift
+//
+//
+//  Created by 김인섭 on 10/6/23.
+//
+
+import Combine
+
+public extension Subscribers.Completion {
+    
+    var error: Error? {
+        
+        guard case let .failure(failure) = self else {
+            return nil
+        }
+        return failure
+    }
+}
