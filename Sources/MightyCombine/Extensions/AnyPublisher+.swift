4b206a60be08896acfb45ddb636e21cb369b4848
insub <vb15@kakao.com>
2023-10-06 13:35:32 +0900

    [Feat] Mock DI (#1)
    * Mock DI
    
    * Mock DI
    
    * [Refactor] Mock struct -> enum
    
    * [Refactor] NetworkMock
    
    * [Feat] Mock DI
    
    * update README

--- Sources/MightyCombine/Extensions/AnyPublisher+.swift
+++ Sources/MightyCombine/Extensions/AnyPublisher+.swift
@@ -9,25 +9,39 @@ import Foundation
 import Combine
 
 public extension AnyPublisher {
+    
+    static func mock<T>(_ mock: NetworkMock<T>) -> AnyPublisher<T, Error> {
+        switch mock {
+        case .success(let model):
+            return Just(model)
+                .setFailureType(to: Error.self)
+                .eraseToAnyPublisher()
+        case .fail(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+    }
 
      var asyncThrows: Output {
         get async throws {
             try await withCheckedThrowingContinuation { continuation in
                 var cancellable: AnyCancellable?
+                var finishedWithoutValue = true
                 cancellable = first()
                     .subscribe(on: DispatchQueue.main)
                     .sink { completion in
                         switch completion {
                         case .finished:
-                            break
+                            if finishedWithoutValue {
+                                continuation.resume(throwing: AnyPublisherError.finishedWithoutValue)
+                            }
                         case .failure(let error):
                             continuation.resume(throwing: error)
                         }
                         cancellable?.cancel()
                     } receiveValue: { value in
+                        finishedWithoutValue = false
                         continuation.resume(with: .success(value))
                     }
-                
             }
         }
     }
@@ -39,9 +53,14 @@ public extension AnyPublisher {
                 .setFailureType(to: Failure.self)
                 .eraseToAnyPublisher()
         case .fail(let error):
-            return Fail(error: error as! Failure)
+            guard let error = error as? Failure
+            else { return Empty().eraseToAnyPublisher() }
+            return Fail(error: error)
                 .eraseToAnyPublisher()
         }
     }
 }
 
+enum AnyPublisherError: Error {
+    case finishedWithoutValue
+}
