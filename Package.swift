a016b3f5f3dc44a006b5c2d08934c3b5a9bc9327
insub <vb15@kakao.com>
2023-10-09 19:44:02 +0900

    add New Product - MightyUIKit

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MightyCombine",
             targets: [
@@ -21,17 +20,28 @@ let package = Package(
             targets: [
                 "MightySwift"
             ]
+        ),
+        .library(
+            name: "MightyUIKit",
+            targets: [
+                "MightyUIKit"
+            ]
         )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        
+        // Product
         .target(
             name: "MightyCombine"
         ),
         .target(
             name: "MightySwift"
         ),
+        .target(
+            name: "MightyUIKit"
+        ),
+        
+        // Tests
         .target(
             name: "TestSource",
             dependencies: [
