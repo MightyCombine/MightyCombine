05816e80f3a8c5eb41265c95ba4aaed0c83e1ba7
insub <vb15@kakao.com>
2023-10-11 00:31:59 +0900

    edit: MightyCombine 추가 시 MightySwift 도 import 가능

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
         .library(
             name: "MightyCombine",
             targets: [
-                "MightyCombine"
+                "MightyCombine",
+                "MightySwift"
             ]
         ),
         .library(
