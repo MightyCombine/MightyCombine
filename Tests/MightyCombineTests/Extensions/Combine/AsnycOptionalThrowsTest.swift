f52abab99011fdcd23e6a7bd5693c35c3f193846
insub <vb15@kakao.com>
2023-10-16 13:38:37 +0900

    feat: asyncOptionalTry

---
+++ Tests/MightyCombineTests/Extensions/Combine/AsnycOptionalThrowsTest.swift
@@ -0,0 +1,33 @@
+//
+//  AsnycOptionalThrowsTest.swift
+//  
+//
+//  Created by 김인섭 on 10/16/23.
+//
+
+import XCTest
+import Combine
+@testable import MightyCombine
+
+final class AsnycOptionalThrowsTest: XCTestCase {
+
+    func test_Just_return_Value() throws {
+        Task {
+            let value = try await Just("Value")
+                .receive(on: DispatchQueue.main)
+                .asyncThrows
+            
+            XCTAssertEqual(value, "Value")
+        }
+    }
+    
+    func test_Fail_return_nil() throws {
+        Task {
+            let value = await Just("Value")
+                .receive(on: DispatchQueue.main)
+                .asyncOptionalTry
+            
+            XCTAssertNil(value)
+        }
+    }
+}
