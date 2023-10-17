919ab1c4d0036bc8acdb212c6d85631f62dd27cd
insub <vb15@kakao.com>
2023-10-17 10:35:01 +0900

    feat: asyncReplaceError

--- Sources/MightyCombine/Extensions/Combine/Publisher+.swift
+++ Sources/MightyCombine/Extensions/Combine/Publisher+.swift
@@ -58,6 +58,29 @@ public extension Publisher {
         }
     }
     
+    func asyncReplaceError(with replaceValue: Output) async -> Output? {
+        await withCheckedContinuation { continuation in
+            var cancellable: AnyCancellable?
+            var finishedWithoutValue = true
+            cancellable = first()
+                .sink(receiveCompletion: { compltion in
+                    switch compltion {
+                    case .finished:
+                        if finishedWithoutValue {
+                            continuation.resume(returning: replaceValue)
+                        }
+                    case .failure(_):
+                        continuation.resume(returning: replaceValue)
+                    }
+                    cancellable?.cancel()
+                }, receiveValue: { value in
+                    finishedWithoutValue = false
+                    continuation.resume(returning: value)
+                })
+        }
+    }
+        
+    
     @available(macOS 10.15, *)
     func asyncMap<T>(_ transform: @escaping (Output) async -> T) -> Publishers.FlatMap<Future<T, Failure>, Self> {
         flatMap { value in
