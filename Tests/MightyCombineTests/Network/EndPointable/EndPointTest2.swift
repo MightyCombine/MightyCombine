b23f9ee0f11ac5cc8d8c7f8fef27b4e9c0f19033
SangWoo Shin <ssw522b@naver.com>
2024-01-25 11:01:53 +0900

    Feat: Add EncodableType Body - #87 (#88)
    * Feat: Add EncodableType Body - #87
    
    * Chore:  Update ReadMe
    
    * Test: EncodableType Body TestCode추가
    
    * Docs: Update readme
    
    ---------
    
    Co-authored-by: insub <vb15@kakao.com>

---
+++ Tests/MightyCombineTests/Network/EndPointable/EndPointTest2.swift
@@ -0,0 +1,85 @@
+//
+//  EndPointTest2.swift
+//
+//
+//  Created by iOS신상우 on 1/24/24.
+//
+
+import XCTest
+@testable import MightyCombine
+
+final class EndPointTest2: XCTestCase {
+    
+    // Given
+    struct EncodableTestBody: Codable {
+        let id: Int
+        let hireable: Bool
+    }
+    
+    let baseUrl = "https://api.github.com"
+    let paths = [
+        "/users",
+        "/octocat"
+    ]
+    let queries = [
+        "type": "User",
+        "login": "octocat"
+    ]
+    let headers = [
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28"
+    ]
+    let body: [String: Any] = [
+        "id": 1,
+        "hireable": false
+    ]
+    
+    let encodableBody: EncodableTestBody = .init(id: 1, hireable: false)
+    
+    // When
+    var request: URLRequest {
+        EndPoint
+            .init(baseUrl)
+            .urlPaths(paths)
+            .urlQueries(queries)
+            .httpHeaders(headers)
+            .httpBody(encodableBody)
+            .httpMethod(.get)
+            .urlRequest
+    }
+    
+    // Then
+    func test_AbosoluteUrlString() {
+        
+        guard let absoluteUrl = request.url?.absoluteString else { return }
+        
+        let expect1 = baseUrl + "/users" + "/octocat" + "?type=User&login=octocat"
+        let expect2 = baseUrl + "/users" + "/octocat" + "?login=octocat&type=User"
+        
+        XCTAssertTrue(absoluteUrl == expect1 || absoluteUrl == expect2)
+    }
+    
+    func test_Header() {
+        guard let header = request.allHTTPHeaderFields else { return }
+        XCTAssertEqual(header, headers)
+    }
+    
+    func test_body() {
+        
+        guard let bodyData = request.httpBody,
+              let requestBody = try? JSONDecoder().decode(EncodableTestBody.self, from: bodyData) else {
+            XCTFail("Body is empty")
+            return
+        }
+        
+        XCTAssertEqual(requestBody.id, 1)
+        XCTAssertEqual(requestBody.hireable, false)
+    }
+    
+    func test_Method() {
+        
+        guard let method = request.httpMethod else { return }
+        
+        XCTAssertEqual(method, "GET")
+    }
+}
